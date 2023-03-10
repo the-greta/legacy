46e3b404a94b8c3955e63e85cb2b5deed23bff89
Dongwon Kang <dw3624@gmail.com>
2023-03-10 13:55:53 +0900

    Feat: add GA code

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -4,7 +4,6 @@ export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? "";
 export const existsGaID = GA_ID !== "";
 
 // PV 측정
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (path) => {
   window.gtag("config", GA_ID, {
     // debug_mode: true,
@@ -13,7 +12,6 @@ export const pageview = (path) => {
 };
 
 // GA 이벤트 발화
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value = "" }) => {
   if (!existsGaID) {
     return;
